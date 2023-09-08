932458f49c85ab5ab0999f5e706bf89cd12828d1
ErikJava <112395602+ErikJava@users.noreply.github.com>
2023-09-08 16:13:06 +0200

    Update

--- app/src/main/java/com/example/loginactivity/LoginFragment.java
+++ app/src/main/java/com/example/loginactivity/LoginFragment.java
@@ -1,42 +1,34 @@
 package com.example.loginactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public LoginFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static LoginFragment newInstance(String param1, String param2) {
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
@@ -47,18 +39,83 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnmain = view.findViewById(R.id.mainBtn2);
+        btnmain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnsubmit = view.findViewById(R.id.submitBtn);
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collectUserData();
+                Toast.makeText(getActivity(), "Information sent!", Toast.LENGTH_SHORT).show();
+                clearUserInputFields();
+            }
+        });
+    }
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
+
+    private void collectUserData() {
+        EditText nameEditText = getView().findViewById(R.id.names);
+        String fullName = nameEditText.getText().toString();
+
+        EditText ageEditText = getView().findViewById(R.id.age);
+        String age = ageEditText.getText().toString();
+
+        EditText phoneEditText = getView().findViewById(R.id.phone);
+        String phone = phoneEditText.getText().toString();
+
+        CheckBox conditionsCheckBox = getView().findViewById(R.id.conditions);
+        boolean conditionsChecked = conditionsCheckBox.isChecked();
+
+        RadioGroup radioGroup = getView().findViewById(R.id.radioGroup);
+        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+        RadioButton selectedRadioButton = getView().findViewById(selectedRadioButtonId);
+        String selectedRadioText = "";
+
+        if (selectedRadioButton != null) {
+            selectedRadioText = selectedRadioButton.getText().toString();
+        }
+    }
+
+
+    private void clearUserInputFields() {
+        EditText nameEditText = getView().findViewById(R.id.names);
+        EditText ageEditText = getView().findViewById(R.id.age);
+        EditText phoneEditText = getView().findViewById(R.id.phone);
+
+
+        nameEditText.setText("");
+        ageEditText.setText("");
+        phoneEditText.setText("");
+
+        CheckBox conditionsCheckBox = getView().findViewById(R.id.conditions);
+        conditionsCheckBox.setChecked(false);
+
+        RadioGroup radioGroup = getView().findViewById(R.id.radioGroup);
+        radioGroup.clearCheck();
+
+    }
+
 }
--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -1,32 +1,60 @@
 package com.example.loginactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText loginText, passwordText;
     Button btnLogin;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginText = (EditText) findViewById(R.id.login);
-        passwordText = (EditText) findViewById(R.id.password);
+        loginText = findViewById(R.id.login);
+        passwordText = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.buttonLogin);
 
-        btnLogin = (Button) findViewById(R.id.buttonLogin);
+        Button btnfragment = findViewById(R.id.fragmentBtn);
+        btnfragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction()
+                        .replace(R.id.Mainpage, LoginFragment.class, null)
+                        .commit();
+            }
+        });
+
+
+        sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+
+
+        String savedUsername = sharedPreferences.getString("username", "");
+        String savedPassword = sharedPreferences.getString("password", "");
+
+        if (!savedUsername.isEmpty() || !savedPassword.isEmpty()) {
+            loginText.setText(savedUsername);
+            passwordText.setText(savedPassword);
+        }
+
+
+        loginText.addTextChangedListener(textWatcher);
+        passwordText.addTextChangedListener(textWatcher);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 FragmentManager fm = getSupportFragmentManager();
@@ -34,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 String password = passwordText.getText().toString();
 
                 if (username.equals("admin") && password.equals("123")) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.apply();
+
                     Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
                     fm.beginTransaction()
                             .replace(R.id.Mainpage, LoginFragment.class, null)
@@ -41,8 +74,29 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
     }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String username = loginText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("username", username);
+            editor.putString("password", password);
+            editor.apply();
+        }
+    };
 }
+
